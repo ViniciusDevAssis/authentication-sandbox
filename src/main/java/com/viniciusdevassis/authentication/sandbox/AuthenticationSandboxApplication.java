bbdd95cbd86e7f7bc936324bb3e688d67cfd3d6d
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-02-18 16:44:16 -0300

    Início

---
+++ src/main/java/com/viniciusdevassis/authentication/sandbox/AuthenticationSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.authentication.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSandboxApplication.class, args);
+	}
+
+}
